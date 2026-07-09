90983bd231a331c79ffd391e82ff512979aa5cea
riddimbossy-prog <riddimbossy@gmail.com>
2026-07-09 22:38:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
      back to the last cached data when offline.
    Bump CACHE_VERSION whenever you ship a new index.html/banker-engine.js so
    users get the update instead of a stale cached shell. */
-const CACHE_VERSION = "predict2u-v136";
+const CACHE_VERSION = "predict2u-v137";
 const SHELL = [
   "./index.html",
   "./board.html",
